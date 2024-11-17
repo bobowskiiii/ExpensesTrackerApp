c4810a11cedf15819fa0783198bc60c25b1d5f2a
bobowskiii <sbobowskiii@gmail.com>
2024-11-17 13:17:32 +0100

    Initial commit to new repository

--- obj/Debug/net9.0/ExpenseTrackerAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExpenseTrackerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8af870b722aeed61af02f4425b443c3bbf66a02")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
